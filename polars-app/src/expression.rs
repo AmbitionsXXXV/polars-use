979dbcf09f7f54575c5da49f09e441606d9c67b5
etcetera <yilonglei790@gmail.com>
2024-07-24 18:05:43 +0800

    chore: 优化代码格式

---
+++ polars-app/src/expression.rs
@@ -0,0 +1,33 @@
+use anyhow::Result;
+use polars::frame::DataFrame;
+use polars_rw::expression::{
+  polars_filter, polars_group_by, polars_select, polars_with_column,
+  polars_with_columns,
+};
+
+pub fn run_polars_expression(df: &DataFrame) -> Result<()> {
+  // ======polars 表达式======
+  // 1. select
+  // 2. filter
+  // 3. with_columns
+  // 4. group_by
+
+  // select
+  polars_select(df)?;
+
+  // filter
+  polars_filter(df)?;
+
+  // with_columns
+  // 一次添加多列
+  polars_with_columns(df)?;
+
+  // with_column
+  // 一次添加一列
+  polars_with_column(df)?;
+
+  // group_by
+  polars_group_by()?;
+
+  Ok(())
+}
--- polars-app/src/main.rs
+++ polars-app/src/main.rs
@@ -1,34 +1,12 @@
 use anyhow::{Ok, Result};
-use polars_rw::expression::{
-  polars_filter, polars_group_by, polars_select, polars_with_column,
-  polars_with_columns,
-};
+use expression::run_polars_expression;
+
+mod expression;
 
 fn main() -> Result<()> {
   let df = polars_rw::polars_read()?;
 
-  // ======polars 表达式======
-  // 1. select
-  // 2. filter
-  // 3. with_columns
-  // 4. group_by
-
-  // select
-  polars_select(&df)?;
-
-  // filter
-  polars_filter(&df)?;
-
-  // with_columns
-  // 一次添加多列
-  polars_with_columns(&df)?;
-
-  // with_column
-  // 一次添加一列
-  polars_with_column(&df)?;
-
-  // group_by
-  polars_group_by()?;
+  run_polars_expression(&df)?;
 
   Ok(())
 }
